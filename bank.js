eb1f53ab6da0328025782d68274e1e6f354ecac2
femiclassic11-ai <femiclassic11@gmail.com>
2025-10-26 23:46:15 +0100

    top

---
+++ bank.js
@@ -0,0 +1,99 @@
+
+
+
+//array of customer info objects
+
+const customerAccounts = [
+    {
+        accountId: 2501,
+        accountHolder: "Ogunbunmi Oluwafemi",
+        balance: 100000,
+        type: "Savings"
+    },
+    {
+        accountId: 2502,
+        accountHolder: "Og-george",
+        balance: 95000,
+        type: "Checking"
+    },
+    {
+        accountId: 2503,
+        accountHolder: "Adebola Moyinoluwa",
+        balance: 93000,
+        type: "Saving for my new house"
+    }
+];
+
+const BANK_NAME = "My Bank Inc"
+
+//function that check balance
+
+function checkBalance(accountId) {
+    const account = customerAccounts.find((acc) => acc.accountId === accountId);
+    if (account) {
+        console.log(`${account.accountHolder}'s balance:  ₦${account.balance.toFixed(2)}`);
+    } else {
+        console.log("Account not found");
+    }
+}
+
+//function to deposit money
+
+function deposit(accountId, amount) {
+    const account = customerAccounts.find((acc) => acc.accountId === accountId);
+    if (account) {
+        if (amount > 0) {
+            account.balance += amount;
+            console.log(`Deposit succesful ₦${amount.toFixed(2)}
+            added to ${account.accountHolder}'s account at ${BANK_NAME}.`);
+            console.log(`New balance: ₦${account.balance.toFixed(2)}`);
+        } else {
+            console.log("Please put in a vaid amount.");
+        }
+    } else {
+        console.log("Account not found");
+    }
+}
+
+//function to withdraw money
+
+function withdraw(accountId, amount) {
+    const account = customerAccounts.find((acc) => acc.accountId === accountId);
+    if (account) {
+        const FEE_RATE = 0.01;
+        const fee = amount * FEE_RATE;
+        if (amount + fee <= account.balance) {
+            account.balance -= amount + fee;
+            console.log(
+                `Withdrawal succesful! ₦${amount.toFixed(2)} withdrawn (fee: ₦${fee.toFixed(2)}).`
+             );
+             console.log(`New balance: ₦${account.balance.toFixed(2)}`);
+        } else {
+            console.log(`Insufficient funds. Unable to withdraw ₦${amount.toFixed(2)}`);
+        }
+    } else {
+        console.log("Account not found.");
+    }
+}
+
+console.log();
+checkBalance(2501);
+checkBalance(2502);
+
+console.log();
+deposit(2501, 500.00);
+
+console.log();
+withdraw(2501, 350.00);
+
+console.log();
+withdraw(2502, 100000.00);
+
+console.log();
+checkBalance(2501);
+checkBalance(2502);
+
+
+
+
+
